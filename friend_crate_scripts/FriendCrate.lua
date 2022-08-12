695cf59aac32c7ad6a61a4b7be4cc43e7ee5c7b7
thicco-catto <cristobal.martin01@gmail.com>
2022-08-12 13:36:41 +0200

    Added lost baby

--- friend_crate_scripts/FriendCrate.lua
+++ friend_crate_scripts/FriendCrate.lua
@@ -9,7 +9,7 @@ end
 
 local Constants = loadFile("friend_crate_scripts/Constants")
 
-local SpiderBaby = loadFile("friend_crate_scripts/friends/troll_baby")
+local SpiderBaby = loadFile("friend_crate_scripts/friends/lost_baby")
 
 ---@param player EntityPlayer
 function FriendCrate:OnFamiliarCache(player)
--- friend_crate_scripts/friends/lost_baby.lua
+++ friend_crate_scripts/friends/lost_baby.lua
@@ -17,7 +17,8 @@ end
 
 ---@param familiar EntityFamiliar
 function LostBaby:OnUpdate(familiar)
-    if game:GetFrameCount() % 5 ~= 0 then return end
+    if game:GetFrameCount() % 7 ~= 0 then return end
+    if familiar.Velocity:Length() < 1 then return end
 
     Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_RED, 0, familiar.Position + Vector(0, -10), Vector.Zero, familiar)
 end
